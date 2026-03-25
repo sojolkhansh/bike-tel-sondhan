ebc3b0d06f9469ce970f9717012f6851120518ad
sojolkhansh <sojolkhansh@gmail.com>
2026-03-25 22:03:17 +0600

    Update MapView.js

--- components/MapView.js
+++ components/MapView.js
@@ -3,7 +3,6 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { db } from "../lib/firebase";
 import { collection, onSnapshot } from "firebase/firestore";
 import PumpCard from "./PumpCard";
-import "leaflet/dist/leaflet.css";
 
 export default function MapView() {
   const [pumps, setPumps] = useState([]);
@@ -21,15 +20,32 @@ export default function MapView() {
   }, []);
 
   return (
-    <MapContainer center={[23.6, 90.3]} zoom={7} style={{ height: "80vh" }}>
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      {pumps.map(p => (
-        <Marker key={p.id} position={[p.lat, p.lng]}>
-          <Popup>
-            <PumpCard pump={p} />
-          </Popup>
-        </Marker>
-      ))}
-    </MapContainer>
+    <>
+      {/* 🔥 ADD BUTTON */}
+      <a href="/add" style={{
+        position: "fixed",
+        top: 10,
+        right: 10,
+        background: "green",
+        color: "#fff",
+        padding: "10px",
+        borderRadius: "10px",
+        zIndex: 1000
+      }}>
+        ➕ Add Pump
+      </a>
+
+      <MapContainer center={[23.6, 90.3]} zoom={7} style={{ height: "90vh" }}>
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+        {pumps.map(p => (
+          <Marker key={p.id} position={[p.lat, p.lng]}>
+            <Popup>
+              <PumpCard pump={p} />
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+    </>
   );
 }
