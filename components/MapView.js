1c1fb2d7ff264fdc1a579d44daeb577d206da98e
sojolkhansh <sojolkhansh@gmail.com>
2026-03-25 23:34:35 +0600

    Update MapView.js

--- components/MapView.js
+++ components/MapView.js
@@ -9,57 +9,40 @@ export default function MapView() {
   const [pumps, setPumps] = useState([]);
 
   useEffect(() => {
-    const unsub = onSnapshot(collection(db, "pumps"), (snapshot) => {
-      const data = snapshot.docs.map(doc => ({
-        id: doc.id,
-        ...doc.data()
-      }));
-      setPumps(data);
+    const unsub = onSnapshot(collection(db, "pumps"), (snap) => {
+      setPumps(snap.docs.map(doc => ({ id:doc.id, ...doc.data() })));
     });
-
     return () => unsub();
   }, []);
 
   return (
-    <div style={{ position: "relative", height: "100%", width: "100%" }}>
+    <div style={{ height:"100%", width:"100%", position:"relative" }}>
 
-      {/* ➕ Add Button */}
       <a href="/add" style={{
-        position: "absolute",
-        top: 15,
-        right: 15,
-        background: "#22c55e",
-        color: "#fff",
-        padding: "10px 15px",
-        borderRadius: "10px",
-        zIndex: 1000,
-        textDecoration: "none"
+        position:"absolute",
+        top:10,
+        right:10,
+        background:"#22c55e",
+        padding:"10px",
+        borderRadius:"10px",
+        color:"#fff",
+        zIndex:1000
       }}>
-        ➕ Add Pump
+        ➕ Add
       </a>
 
-      <MapContainer
-        center={[23.6850, 90.3563]} // Bangladesh center
-        zoom={7}
-        minZoom={6}
-        maxZoom={18}
-        scrollWheelZoom={true}
-        style={{ height: "100%", width: "100%" }}
-      >
+      <MapContainer center={[23.6,90.3]} zoom={7} style={{height:"100%"}}>
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
 
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-
-        {pumps.map(p => (
-          <Marker key={p.id} position={[p.lat, p.lng]}>
+        {pumps.map(p=>(
+          <Marker key={p.id} position={[p.lat,p.lng]}>
             <Popup>
-              <PumpCard pump={p} />
+              <PumpCard pump={p}/>
             </Popup>
           </Marker>
         ))}
-      </MapContainer>
 
+      </MapContainer>
     </div>
   );
 }
